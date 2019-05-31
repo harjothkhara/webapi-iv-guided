44f6cf878de14e3b72c5299024387d247a4e4268
harjothkhara <harjoth.khara@gmail.com>
2019-05-30 18:31:56 -0700

    made port dynamic

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ require('dotenv').config(); //reads .env and merges it into process.env
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
